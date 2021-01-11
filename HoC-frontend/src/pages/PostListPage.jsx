f1cb918f03dac1ebbdb004574cdc7f887c39f1ac
Kim-Jaemin420 <jany240@naver.com>
2021-01-11 16:43:23 +0900

    postlist

--- HoC-frontend/src/pages/PostListPage.jsx
+++ HoC-frontend/src/pages/PostListPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Button from '../components/common/Button';
 import StyledContainer from '../components/common/Container';
 import Header from '../components/common/Header';
@@ -12,19 +11,6 @@ const PostListPage = () => {
         <Button>버튼</Button>
         </StyledContainer>
     </div>
-=======
-import Header from '../components/common/Header';
-import StyledContainer from '../components/common/Container';
-
-const PostListPage = () => {
-  return (
-    <>
-      <Header />
-      <StyledContainer>
-        <div>안녕하세요</div>
-        </StyledContainer>
-    </>
->>>>>>> b1e8c61480c1c4bd3e036501ff50a19a63ad3381
   );
 };
 
