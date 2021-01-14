590b90f6a25ca1d5d4acbe21b5aa61df1e9b8778
Kim-Jaemin420 <jany240@naver.com>
2021-01-14 16:17:17 +0900

    내가 쓴 글 컨테이너

--- HoC-frontend/src/pages/PostListPage.jsx
+++ HoC-frontend/src/pages/PostListPage.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../components/common/Button';
 import StyledContainer from '../components/common/Container';
 import Header from '../components/common/Header';
+import { Auth } from '../contexts/store';
 
 const PostListPage = () => {
+  const { AuthState, AuthDispatch } = useContext(Auth);
+  console.log(AuthState);
   return (
     <div>
       <Header />
-        <Button>버튼</Button>
-        <Link to="/write" >글쓰기</Link>
+      <Button>버튼</Button>
+      {AuthState.company.username && (<Link to="/write" >글쓰기</Link>)}
+      {AuthState.company.username && (<Link to="/mypage" >내가 쓴 글</Link>)}
       <StyledContainer>
       </StyledContainer>
     </div>
