3afc01807880511a0b2f5b8203181e75b6158e2e
youngseo-im <gg2922914@gmail.com>
2021-01-21 16:02:33 +0900

    get 해오는 url 변경

--- HoC-backend/src/api/posts/index.js
+++ HoC-backend/src/api/posts/index.js
@@ -7,8 +7,9 @@ export const posts = new Router();
 posts.get('/', postsCtrl.list);
 posts.post('/', checkLoggedIn, postsCtrl.write);
 
+
 export const post = new Router(); // api/posts/:id
-post.get('/', postsCtrl.read);
+post.get('/:id', postsCtrl.read);
 post.delete('/:id', postsCtrl.remove);
 
 // post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
